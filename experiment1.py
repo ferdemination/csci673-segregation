cfe5719cb15f796b89f7afff77f55dc512e5ec63
VasilisVar00 <billyvarsamis@gmail.com>
2025-05-13 15:54:10 -0700

    experiments and results

---
+++ experiment1.py
@@ -0,0 +1,43 @@
+from zhang import Grid
+from zhang import draw_grid
+import pygame
+import random
+import csv
+import metriccomputations
+
+
+# N x N grid size
+N = 30
+num_races = 3                          
+colors = ["white", "black", "orange"]
+# defines a color->index coreespondence
+color_dict = {c:colors.index(c) for c in colors}
+
+p = [[-1,-1,-1], [-1,-1,-2], [1,-1,1]]
+
+fracs = [ _ / 10 for _ in range(1,10)]
+num_occupants = 300
+num_orange = [int( _ * num_occupants) for _ in fracs]
+
+for no in num_orange:
+    races = {i: (num_occupants if i == "white" or i == "black" else no)
+             for i in colors }
+    print(races)
+    g = Grid(N=N,p=p,color_dict=color_dict,colors=races)
+    running = True
+    K=5
+    while running:
+        metrics = metriccomputations.compute_metrics(g,colors,K)
+        running = g.next_step()
+    with open(f"results1.csv", "a", newline= '') as file:
+        writer = csv.writer(file)
+        field = ["num_of_orange","race","avg_dist","K_div","multi_racial_fraction"]
+        writer.writerow(field)
+    with open("results1.csv", "a", newline='') as file:
+        writer = csv.writer(file)
+        for col in colors:
+            data = metrics[col]
+            writer.writerow([ no, col, data['avg_distance'], data['diversity'], data['edge_fraction'] ])
+
+
+
--- experiment2.py
+++ experiment2.py
@@ -2,64 +2,52 @@ from zhang import Grid
 from zhang import draw_grid
 import pygame
 import random
+import csv
 import metriccomputations
 
-colors = list(pygame.color.THECOLORS.keys())
-print (colors)
-random.shuffle(colors)
+
 # N x N grid size
 N = 30
-num_races = 8 # must be multiple of 2 for this experiment                                      
-races = colors[:num_races] #take the first num_races colors
-num_occupants = 100
-colors = {r:num_occupants for r in races}
-
+num_races = [2,4,6,8,10]                            
+matching = {"white":"black", "red":"blue", "orange":"green", "brown":"yellow", "purple":"pink"}
+colors = []
+for r in matching:
+    colors.append(r)
+    colors.append(matching[r])
+print(colors)
 # defines a color->index coreespondence
-color_dict = {c:races.index(c) for c in races}
-matching = {races[i]:races[i+1] for i in range(0,len(races),2)}
-print(matching)
+color_dict = {c:colors.index(c) for c in colors}
+print(color_dict)
+
+num_occupants = 100
+
+
 
 p = []
 for (race,mrace) in matching.items():
-    cand1 = [0 for _ in races]
-    cand2 = [0 for _ in races]
+    cand1 = [0 for _ in colors]
+    cand2 = [0 for _ in colors]
     cand1[color_dict[mrace]] = -1
     cand2[color_dict[race]] = -1
     p.append(cand1)
     p.append(cand2)
-print(p)
-g = Grid(N=N,p=p,color_dict=color_dict,colors=colors)
 
-running = True
-K=5
-#DISPLAY CODE
-FPS = 30
-
-CELL_SIZE = 20
-WIDTH = HEIGHT = N * CELL_SIZE
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
-#END DISPLAY
-while running:
-    #DISPLAY CODE:
-    screen.fill((0, 0, 0))
-    draw_grid(screen, g.grid)
-    pygame.display.flip()
-    clock.tick(FPS)
-    #END DISPLAY
-    metrics = metriccomputations.compute_metrics(g,races,K)
-    running = g.next_step()
-    for race in races:
-            name = race
-            data = metrics[race]
-            print(f"{name}:")
-            print(f"  Avg distance to nearest different race: {data['avg_distance']:.2f}")
-            print(f"  K={K} neighborhood diversity: {data['diversity']:.2%}")
-            print(f"  Multiracial edge fraction: {data['edge_fraction']:.2%}\n")
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-pygame.quit()
-
+for r in num_races:
+    num_occupants = 800 // r
+    curr_colors = colors[:r]
+    races = {i:num_occupants for i in curr_colors}
+    g = Grid(N=N,p=p,color_dict=color_dict,colors=races)
+    running = True
+    K=5
+    while running:
+        metrics = metriccomputations.compute_metrics(g,curr_colors,K)
+        running = g.next_step()
+    with open(f"results2.csv", "a", newline= '') as file:
+        writer = csv.writer(file)
+        field = ["num_of_races","race","avg_dist","K_div","multi_racial_fraction"]
+        writer.writerow(field)
+    with open("results2.csv", "a", newline='') as file:
+        writer = csv.writer(file)
+        for col in curr_colors:
+            data = metrics[col]
+            writer.writerow([ r, col, data['avg_distance'], data['diversity'], data['edge_fraction'] ])
